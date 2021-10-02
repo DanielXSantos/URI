e5c67c7054f7c85b80e61682ea489ef5a3ed592e
Robson Vieira <robson.ppmg@gmail.com>
2021-10-02 19:39:50 -0300

    Solving problem 1002

---
+++ src/main/java/session1/P1002.java
@@ -0,0 +1,16 @@
+package session1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1002 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            Double n = 3.14159;
+            Double a = Double.parseDouble(br.readLine());
+
+        System.out.printf("A=%.4f\n" , n * (a * a));
+    }
+}
