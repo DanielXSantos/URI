4c10fcee8d8e74d2f696b4ce30dcb0fa774e9a83
Robson Vieira <robson.ppmg@gmail.com>
2021-10-02 19:48:30 -0300

    Fixing identation

--- src/main/java/session1/P1002.java
+++ src/main/java/session1/P1002.java
@@ -8,8 +8,8 @@ public class P1002 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            Double n = 3.14159;
-            Double a = Double.parseDouble(br.readLine());
+        Double n = 3.14159;
+        Double a = Double.parseDouble(br.readLine());
 
         System.out.printf("A=%.4f\n" , n * (a * a));
     }
