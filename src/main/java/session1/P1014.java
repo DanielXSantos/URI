a539a508e175f6a5c9fb122768c562aed4416e35
Robson Vieira <robson.ppmg@gmail.com>
2021-10-04 23:58:31 -0300

    solving problem 1014

---
+++ src/main/java/session1/P1014.java
@@ -0,0 +1,19 @@
+package session1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1014 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer km = Integer.parseInt(br.readLine());
+        Double fuel = Double.parseDouble(br.readLine());
+
+        System.out.printf("%.3f km/l\n" , km / fuel);
+
+    }
+}
